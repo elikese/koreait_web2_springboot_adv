060f6e1eeb5e2012b3c1c254be731134e6ac1332
elikese <elikese@gmail.com>
2026-06-12 20:54:04 +0900

    feat: room

--- src/main/java/com/study/websocket/DemoApplication.java
+++ src/main/java/com/study/websocket/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-    //  bit.ly/koreait-web
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/study/websocket/controller/ChatController.java
+++ src/main/java/com/study/websocket/controller/ChatController.java
@@ -1,8 +1,11 @@
 package com.study.websocket.controller;
 
 import com.study.websocket.dto.ChatMessageResponse;
+import com.study.websocket.dto.MessageRequest;
 import com.study.websocket.dto.NotificationRequest;
+import com.study.websocket.service.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -15,6 +18,7 @@ import java.time.LocalDateTime;
 @Controller
 @RequiredArgsConstructor
 public class ChatController {
+    private final ChatService chatService;
 
     // 클라이언트는 localhost:8080/app/chat url로 stomp 메세지를 전송해야함
     // SendTo는 /topic/chat을 구독한다면 리턴한 데이터를 메세지로 받아 볼 수 있음 
@@ -30,5 +34,36 @@ public class ChatController {
                 LocalDateTime.now().toString()
         );
     }
+
+    // 서로다른 목적지에 서로다른 메세지를 보내는경우
+    // @SendTo으로는 한계가 존재
+    // -> simpMessagingTemplate 사용
+    @MessageMapping("/chat/filter")
+    public void sendFiltered(MessageRequest request, Principal principal) {
+        chatService.sendFiltered(principal.getName(), request.getMessage());
+    }
+
+
+    /*
+        브라우저(클라이언트) 입장에서 할일
+        입장(구독) - /topic/chat/{roomId}
+        보내기(발행) - /app/chat/{roomId}
+    */
+    /*
+        서버 입장에서 할일
+        /app/chat/{roomId} 들어온 데이터를 /topic/chat/{roomId}로 밀어주기
+    */
+    @MessageMapping("/chat/{roomId}")
+    @SendTo("/topic/chat/{roomId}")
+    public ChatMessageResponse sendRoom(
+            @DestinationVariable String roomId, // stomp에서의 pathvariable
+            MessageRequest request, Principal principal
+    ) {
+        // roomId는 목록에서 조회해온 값이 x, STOMP 주소에서 꺼내온 값
+        return new ChatMessageResponse(
+                principal.getName(), request.getMessage(), LocalDateTime.now().toString()
+        );
+    }
+
 
 }
